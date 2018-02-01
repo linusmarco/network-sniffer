57f6711412892f1dd29ba3f220fa75f420ba1cb8
Linus Marco <lmarco163@gmail.com>
2018-01-31 20:06:48 -0500

    add logger

--- src/sniff.js
+++ src/sniff.js
@@ -1,25 +1,50 @@
 const httpServer = require('http-server');
 const ip = require('ip');
+const fs = require('fs');
 
 const arg = process.argv[2];
 const addr = arg ? arg.split(':') : [];
 const host = addr[0] || ip.address();
 const port = addr[1] || 8080;
+const now = new Date();
 
-var server = httpServer.createServer({
-    logFn: logger
-});
+const logFiles = [`./logs/${now.toISOString()}.log`, './logs/current.log'];
+
+class Log {
+    constructor(files, toConsole) {
+        this.files = files;
+        this.toConsole = toConsole || true;
+        this.streams = this.files.map(f => {
+            return fs.createWriteStream(f);
+        });
+
+        this.log = this.log.bind(this);
+        this.logReq = this.logReq.bind(this);
+    }
+
+    log(msg) {
+        if (this.toConsole) {
+            console.log(msg);
+        }
+
+        this.streams.forEach(s => {
+            s.write(msg + '\n');
+        });
+    }
+
+    logReq(req, res) {
+        const date = new Date();
+        const url = req.url;
+        const clientIP = req.connection.remoteAddress;
+        this.log(
+            `[${date}] CLIENT: ${clientIP} | URL: http://${host}:${port}${url}`
+        );
+    }
+}
+
+let log = new Log(logFiles);
+let server = httpServer.createServer({ logFn: log.logReq });
 
 server.listen(port, host, function() {
-    console.log(`listening on http://${host}:${port}/`);
+    log.log(`listening on http://${host}:${port}/`);
 });
-
-function logger(req, res) {
-    const date = new Date();
-    const url = req.url;
-    const clientIP = req.connection.remoteAddress;
-
-    console.log(
-        `[${date}] CLIENT: ${clientIP} | URL: http://${host}:${port}${url}`
-    );
-}
